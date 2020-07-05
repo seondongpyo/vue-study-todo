015981dddd8d68d386b1a1669df9a2efdcfac64e
Omnibus <psd1390@gmail.com>
2020-07-05 13:58:48 +0900

    header 내용 구현

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>header</div>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
 </template>
 
 <script>
@@ -8,6 +10,13 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    /* 
+        scoped : Vue에서 지원하는 속성, 스타일 정의를 해당 컴포넌트에만 적용하겠다는 의미
+    */
+    h1 {
+        color: #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
